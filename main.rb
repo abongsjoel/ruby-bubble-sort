c9273bafe24e023bba1c7a09dca5e96bffde361c
tj-xubuntu <abongsjoel@gmail.com>
2020-12-01 23:45:36 +0100

    Add bubble_sort_by method

--- main.rb
+++ main.rb
@@ -12,7 +12,28 @@ def bubble_sort(array)
     end
   end
 
-  array
+  p array
 end
 
-p bubble_sort([3, 4, 5, 2, 1])
+def bubble_sort_by(array)
+  swapped = true
+
+  while swapped
+    swapped = false
+
+    0.upto(array.length - 2) do |i|
+      if yield(array[i], array[i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+  end
+
+  p array
+end
+
+bubble_sort([3, 4, 5, 2, 1])
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
