cb1449eb99f6707f303ca0a776495cf848c085c0
tj-xubuntu <abongsjoel@gmail.com>
2020-12-01 23:35:18 +0100

    Autocorrect rubocop changes

--- main.rb
+++ main.rb
@@ -1,1 +1,18 @@
+def bubble_sort(array)
+  swapped = true
 
+  while swapped
+    swapped = false
+
+    0.upto(array.length - 2) do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+  end
+
+  array
+end
+
+p bubble_sort([3, 4, 5, 2, 1])
